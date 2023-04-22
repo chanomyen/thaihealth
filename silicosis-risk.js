5e04c94e8f4a65759e890cb1e8a5045d59458a74
Chanon Sermpol <chanon.wsr2@hotmail.com>
2023-04-22 19:40:03 +0700

    added functions file

---
+++ silicosis-risk.js
@@ -0,0 +1,36 @@
+function calculateRisk(dustDensity, workHours, hasDisease, workLocation) {
+    return 9.481 + (317.267 * dustDensity) + (0.712 * workHours) - (2.863 * hasDisease) + (1.374 * workLocation);
+};
+
+function calculateLevel(riskScore) {
+    if ((riskScore >= 1) && (riskScore <= 3)) {
+        return 0;
+    } else if (riskScore >= (4) && (riskScore < 10)) {
+        return 1;
+    } else if ((riskScore >= 10) && (riskScore < 17)) {
+        return 2;
+    } else if ((riskScore >= 17) && (riskScore < 21)) {
+        return 3;
+    } else if ((riskScore >= 21) && (riskScore < 26)) {
+        return 4;
+    } else {
+        return 4;
+    }
+};
+
+const severities = ['ไม่มีนัยสำคัญ', 'ต่ำ', 'ปานกลาง', 'สูง', 'สูงมาก'];
+
+const silicosisRiskForm = document.querySelector('form');
+silicosisRiskForm.addEventListener('submit', async (event) => {
+    event.preventDefault();
+    
+    const dustDensity = document.getElementById('dust_density').value;
+    const workHours = document.getElementById('work_hours').value;
+    const hasDisease = document.getElementById('has_disease').value;
+    const workLocation = document.getElementById('work_location').value;
+
+    const riskScore = calculateRisk(dustDensity, workHours, hasDisease, workLocation);
+    const riskLevel = calculateLevel(riskScore);
+    const alertMessage = `score: ${riskScore}\nlevel: ${riskLevel}\ndisplay: ${severities[riskLevel]}`
+    alert(alertMessage);
+});
