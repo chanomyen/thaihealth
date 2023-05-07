490a8673aa7041d1471e229ae67427642ac97d8f
Chanon Sermpol <chanonse@scg.com>
2023-05-07 21:07:19 +0700

    integrating with LINE API

--- silicosis-register.js
+++ silicosis-register.js
@@ -1,5 +1,6 @@
 const isOtherWorkTypeCheckbox = document.getElementById("otherWorkType");
 const otherWorkTypeInput = document.getElementById("otherWorkTypeValue");
+var lineProfile;
 isOtherWorkTypeCheckbox.addEventListener('change', () => {
     if (isOtherWorkTypeCheckbox.checked) {
         otherWorkTypeInput.disabled = false;
@@ -14,7 +15,7 @@ const userRegisterForm = document.querySelector('form');
 userRegisterForm.addEventListener('submit', async (event) => {
     event.preventDefault();
     const isOtherWorkType = document.getElementById("otherWorkType").checked;
-    
+
     let otherWorkTypeValue = "";
     if (isOtherWorkType) {
         otherWorkTypeValue = document.getElementById("otherWorkTypeValue").value;
@@ -32,8 +33,8 @@ userRegisterForm.addEventListener('submit', async (event) => {
     formDataObject.isCarveStone = document.getElementById("carveStone").checked;
     formDataObject.otherWorkTypeValue = otherWorkTypeValue;
 
-    formDataObject.lineId = "(unique ID)";
-    formDataObject.lineName = "(demo name)";
+    formDataObject.lineId = lineProfile.userId;
+    formDataObject.lineName = lineProfile.displayName;
 
     const json = JSON.stringify(formDataObject); // convert the FormData object to a JSON string
     console.log(json);
@@ -47,27 +48,50 @@ userRegisterForm.addEventListener('submit', async (event) => {
     fetch(url, {
         method: "POST",
         headers: {
-          'Content-Type': 'application/json'
+            'Content-Type': 'application/json'
         },
         body: json
     })
-    .then(response => {
-      console.log('Success:', response);
-      submitBtn.innerHTML = "เสร็จสิ้น";
-      submitBtn.className = "btn btn-success btn-lg";
-      // handle the successful response from the server here
-    })
-    .catch(error => {
-      console.error('Error:', error);
-      submitBtn.innerHTML = "ล้มเหลว";
-      submitBtn.className = "btn btn-danger btn-lg";
-      // handle any errors that occur during the request here
-    });
+        .then(response => {
+            console.log('Success:', response);
+            submitBtn.innerHTML = "เสร็จสิ้น";
+            submitBtn.className = "btn btn-success btn-lg";
+            // handle the successful response from the server here
+        })
+        .catch(error => {
+            console.error('Error:', error);
+            submitBtn.innerHTML = "ล้มเหลว";
+            submitBtn.className = "btn btn-danger btn-lg";
+            // handle any errors that occur during the request here
+        });
 });
 
 const burlBtn = document.getElementById("burl");
 burlBtn.addEventListener("click", () => {
     // console.log(document.getElementById("homeAddress").value);
-    
+
     submitBtn.innerHTML = "กำลังส่งข้อมูล...";
 });
+
+function loadLIFF() {
+    if (!window.LIFF) {
+        const liffScript = document.createElement('script');
+        liffScript.setAttribute('src', 'https://static.line-scdn.net/liff/edge/2.1/sdk.js');
+        liffScript.setAttribute('charset', 'utf-8');
+        document.body.appendChild(liffScript);
+    }
+}
+
+window.onload = async function () {
+    loadLIFF();
+    await liff.init({ liffId: "1660957751-q2MDKokx" });
+    if (liff.isLoggedIn()) {
+        console.log("Logged In!");
+    } else {
+        console.log("Not logged In!");
+        liff.login();
+    }
+
+    const profile = await liff.getProfile();
+    lineProfile = profile;
+};
